e9f035c442c522e65fd614050f46b78cee397a4a
Ryota Misawa <ideodora@gmail.com>
2023-03-16 09:53:12 +0900

    decorated tabs

--- mnt/wrap-node/node/vsc-site/src/lib/components/file-tabs/index.svelte
+++ mnt/wrap-node/node/vsc-site/src/lib/components/file-tabs/index.svelte
@@ -1,5 +1,14 @@
-<div class="bg-stone-900 flex">
-	<div class="py-2 px-4">Welcome</div>
-	<div class="py-2 px-4">Tab2</div>
-	<div class="py-2 px-4">Tab3</div>
+<div class="tabs bg-stone-900 flex gap-x-px text-xs">
+	<div class="tab active py-2 px-4">Welcome</div>
+	<div class="tab py-2 px-4">Tab2</div>
+	<div class="tab py-2 px-4">Tab3</div>
 </div>
+
+<style lang="postcss">
+	.tab {
+		@apply bg-[#343530];
+	}
+	.tab.active {
+		@apply bg-[#272823];
+	}
+</style>
